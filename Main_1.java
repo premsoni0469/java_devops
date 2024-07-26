9e763414455019c0f838332862ac5b74ecc8dc38
premsoni0469 <145101011+premsoni0469@users.noreply.github.com>
2024-07-26 15:19:41 +0530

    Add files via upload

---
+++ Main_1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main_1{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st value: ");
+		int a = sc.nextInt();
+		System.out.println("Enter 2nd value: ");
+		int b = sc.nextInt();	
+		System.out.println("Addition of 2 numbers is: " + (a + b));	
+		// Write below
+	}	
+}
