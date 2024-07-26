7b1f7146c9bd1adb663b1445f9a3a782bacf05f0
Rahool-Singh <153067939+Rahool-Singh@users.noreply.github.com>
2024-07-26 15:34:46 +0530

    TE6_56.java

--- Main_1.java
+++ Main_1.java
@@ -9,6 +9,7 @@ public class Main_1{
 		System.out.println("Addition of 2 numbers is: " + (a + b));
 		System.out.println("Subtraction of 2 numbers is: " + (a - b));
 		System.out.println("Multiplication of 2 numbers is: " + (a * b));
+		// System.out.println("Division of the numbers is: "+(a/b));
 		// Write below
 
 		if(a > b){
